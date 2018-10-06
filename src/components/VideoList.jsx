5763048e7b5eb2d1b2dd423bbed078d573f977cf
rocha <felipe.rocha@monitoratec.com.br>
2018-10-06 14:58:12 -0300

    VideoList return fix

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,20 +3,20 @@ import VideoListItem from './VideoListItem';
 
 import styles from '../styles/VideoList.css';
 
-const VideoList = ({ videos, onVideoSelect }) => (
-  videoItems = videos.map(video => {
-    return <VideoListItem
+const VideoList = ({ videos, onVideoSelect }) => {
+  const videoItems = videos.map(video => (
+    <VideoListItem
       onVideoSelected={onVideoSelect}
       key={video.etag}
       video={video}
     />
-  })
-  
+  ));
+
   return (
     <ul className={styles.listItem}>
       {videoItems}
     </ul>
   );
-);
+};
 
 export default VideoList;
